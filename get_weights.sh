48edc31b42fdfeb9bb4112fcf687177a2b6f9298
Ryan <rmmcnulty9@gmail.com>
2018-05-07 08:28:14 -0400

    Correct a few issues with the bash script.

--- get_weights.sh
+++ get_weights.sh
@@ -5,18 +5,24 @@ set -ueo pipefail
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-WEIGHTS_PATH="$THIS_DIR/darknet_ros/yolo_network_config/weights/yolo_network_config/weights"
+WEIGHTS_PATH="$THIS_DIR/darknet_ros/yolo_network_config/weights"
 
 WEIGHT_NAME="tiny-yolo-voc.weights"
-WEIGHT_FILE="$WEIGHTS_PATH/WEIGHT_NAME"
-if [ -d $"WEIGHT_FILE" ]; then
+WEIGHT_FILE="$WEIGHTS_PATH/$WEIGHT_NAME"
+if [ ! -f $"WEIGHT_FILE" ]; then
     echo "Downloading $WEIGHT_NAME to $WEIGHTS_PATH"
-    wget --quiet "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
+    wget "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
+else
+    echo "Weight file $WEIGHT_FILE already exists"
 fi
 
+echo "------------------"
+
 WEIGHT_NAME="yolo.weights"
-WEIGHT_FILE="$WEIGHTS_PATH/WEIGHT_NAME"
-if [ -d $"WEIGHT_FILE" ]; then
+WEIGHT_FILE="$WEIGHTS_PATH/$WEIGHT_NAME"
+if [ ! -f $"WEIGHT_FILE" ]; then
     echo "Downloading $WEIGHT_NAME to $WEIGHTS_PATH"
-    wget --quiet "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
+    wget "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
+else
+    echo "Weight file $WEIGHT_FILE already exists"
 fi
