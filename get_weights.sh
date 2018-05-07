32f11f69b7381f2f325d38a9a8737f13476d016c
Ryan <rmmcnulty9@gmail.com>
2018-05-07 11:19:55 -0400

    Use the self hosted weights & script cleanup.

--- get_weights.sh
+++ get_weights.sh
@@ -4,38 +4,24 @@ set -ueo pipefail
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+WEIGHT_URL="https://s3-us-west-2.amazonaws.com/public.ecr.guru/3rdparty/yolo"
 
 WEIGHTS_PATH="$THIS_DIR/darknet_ros/yolo_network_config/weights"
 
-WEIGHT_NAME="tiny-yolo-voc.weights"
-WEIGHT_FILE="$WEIGHTS_PATH/$WEIGHT_NAME"
-if [ ! -f $"WEIGHT_FILE" ]; then
-    echo "Downloading $WEIGHT_NAME to $WEIGHTS_PATH"
-    wget "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
-else
-    echo "Weight file $WEIGHT_FILE already exists"
-fi
-
-echo "------------------"
-
-WEIGHT_NAME="yolo.weights"
-WEIGHT_FILE="$WEIGHTS_PATH/$WEIGHT_NAME"
-if [ ! -f $"WEIGHT_FILE" ]; then
-    echo "Downloading $WEIGHT_NAME to $WEIGHTS_PATH"
-    wget "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
-else
-    echo "Weight file $WEIGHT_FILE already exists"
-fi
+NAMES=(
+    "yolo.weights"
+    "tiny-yolo-voc.weights"
+    "yolo-voc.weights"
+    )
 
 
-echo "------------------"
-
-# For testing
-WEIGHT_NAME="yolo-voc.weights"
-WEIGHT_FILE="$WEIGHTS_PATH/$WEIGHT_NAME"
-if [ ! -f $"WEIGHT_FILE" ]; then
-    echo "Downloading $WEIGHT_NAME to $WEIGHTS_PATH"
-    wget "http://pjreddie.com/media/files/$WEIGHT_NAME" -P "$WEIGHTS_PATH"
-else
-    echo "Weight file $WEIGHT_FILE already exists"
-fi
+for WEIGHT_NAME in "${NAMES[@]}"; do
+    echo ""
+    WEIGHT_FILE="${WEIGHTS_PATH}/${WEIGHT_NAME}"
+    if [ -f "${WEIGHT_FILE}" ]; then
+        echo "Weight file ${WEIGHT_FILE} already exists"
+    else
+        echo "Downloading ${WEIGHT_NAME} to ${WEIGHTS_PATH}"
+        wget --quiet "${WEIGHT_URL}/${WEIGHT_NAME}" -P "${WEIGHTS_PATH}"
+    fi
+done
